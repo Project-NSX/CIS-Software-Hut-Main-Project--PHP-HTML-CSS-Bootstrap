be3d9894abfd09d1bcaa2f836aa87fd94273470e
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-02-08 18:13:42 +0000

    Added some generalised php to show information from the Database

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require 'includes/database.php';
 $conn = getDB();
-// Here's our Sql query we'll be running
+// Here's an Sql query we can run (Just used it to test the database connection)
 $sql = "SELECT *
         FROM form";
 
@@ -10,19 +10,54 @@ $sql = "SELECT *
 $results = mysqli_query($conn, $sql);
 
 // Note that this is using three = as it's an "identical comparison" operator
-if($results === false)
-{
-  echo mysqli_error($conn);
+if ($results === false) {
+    echo mysqli_error($conn);
+} else {
+    // For this you can use fetch_row or fetch_all
+    // This will get all of the results and store them in $form_results
+    $form_results = mysqli_fetch_all($results, MYSQLI_ASSOC);
+    // MYSQL_ASSOC will make it return the column names in the table instead of just number indexes
 }
-else
-{
-  // For this you can use fetch_row or fetch_all
-  // This will get all of the results and store them in $form_result
-  $form_result = mysqli_fetch_all($results, MYSQLI_ASSOC);
-  // MYSQL_ASSOC will make it return the column names in the table instead of just number indexes
-}
- ?>
- <?php require 'includes/header.php'; ?>
+?>
+<?php require 'includes/header.php';?>
 <!--HTML HERE-->
 
- <?php require 'includes/footer.php'; ?>
+
+<?php if (empty($form_results)): ?>
+<p>No results were found!</p>
+<?php else: ?>
+<ul>
+    <?php foreach ($form_results as $form_result): ?>
+    <li>
+        <article>
+            <h2>
+                <?=htmlspecialchars($form_result['fName']);?>
+                <?=htmlspecialchars($form_result['lName']);?>
+            </h2>
+
+            <p>
+                <?=htmlspecialchars($form_result['fName']);?>
+                <?=htmlspecialchars($form_result['lName']);?>'s ID is:
+                <?=htmlspecialchars($form_result['idform']);?>
+            </p>
+        </article>
+    </li>
+    <?php endforeach;?>
+</ul>
+<?php endif;?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require 'includes/footer.php';?>
