eca3f7d9db075afedbcbc27c47482fde2c947c5a
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-03-01 22:54:11 +0000

    changed database user to root

--- includes/database.php
+++ includes/database.php
@@ -9,7 +9,7 @@ function getDB()
 {
     $db_host = "localhost";
     $db_name = "va_form";
-    $db_user = "cms_www";
+    $db_user = "root";
     $db_pass = "MyNewPass";
 
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
