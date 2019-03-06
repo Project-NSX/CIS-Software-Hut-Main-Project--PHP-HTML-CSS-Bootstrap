cb91256ed7383127722b7b3a6a2cea046b0e9606
Spectre-NSX <42746611+Spectre-NSX@users.noreply.github.com>
2019-03-06 09:13:35 +0000

    Check this commit - may overwrite changes i've not

--- includes/database.php
+++ includes/database.php
@@ -9,6 +9,7 @@ define('DB_NAME', 'va_form');
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
