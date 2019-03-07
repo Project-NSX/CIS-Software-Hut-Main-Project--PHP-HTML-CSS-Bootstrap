d56c0cb4502456fd7ce28d3cb7e20cbcbce260ec
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-03-07 10:45:39 +0000

    Login system was broken - I fixed it

--- index.php
+++ index.php
@@ -4,7 +4,16 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: academic_landing.php");
+    // Redirect user to welcome page
+    if ($role === "aca") {
+        header("location: academic_landing.php");
+    } elseif ($role === "cm") {
+        header("location: cm_landing.php");
+    } elseif ($role === "hos") {
+        header("location: hos_landing.php");
+    } elseif ($role === "hr") {
+        header("location: hr_landing.php");
+    }
     exit;
 }
 require_once'includes/database.php';
@@ -48,33 +57,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 // Check if username exists, if yes then verify password
                 if (mysqli_stmt_num_rows($stmt) == 1) {
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $role, $school, $username, $password);
+                    mysqli_stmt_bind_result($stmt, $role, $school, $username, $password_param);
                     if (mysqli_stmt_fetch($stmt)) {
+                        if ($password == $password_param) {
+                            // Password is correct, so start a new session
+                            session_start();
 
-                        // Password is correct, so start a new session
-                        session_start();
-
-                        // Store data in session variables
-                        $_SESSION["loggedin"] = true;
-                        $_SESSION["id"] = $id;
-                        $_SESSION["username"] = $username;
-                        $_SESSION["role"] = $role;
-                        $_SESSION["school"] = $school;
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+                            $_SESSION["role"] = $role;
+                            $_SESSION["school"] = $school;
 
 
-                        // Redirect user to welcome page
-                        if ($role === "aca") {
-                            header("location: academic_landing.php");
-                        } elseif ($role === "cm") {
-                            header("location: cm_landing.php");
-                        } elseif ($role === "hos") {
-                            header("location: hos_landing.php");
-                        } elseif ($role === "hr") {
-                            header("location: hr_landing.php");
+                            // Redirect user to welcome page
+                            if ($role === "aca") {
+                                header("location: academic_landing.php");
+                            } elseif ($role === "cm") {
+                                header("location: cm_landing.php");
+                            } elseif ($role === "hos") {
+                                header("location: hos_landing.php");
+                            } elseif ($role === "hr") {
+                                header("location: hr_landing.php");
+                            }
+                        } else {
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
                         }
-                    } else {
-                        // Display an error message if password is not valid
-                        $password_err = "The password you entered was not valid.";
                     }
                 } else {
                     // Display an error message if username doesn't exist
