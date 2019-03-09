a7ef605391d2ae6b4bcc68140eea3651b090ca50
BedwyrPrys <44100623+BedwyrPrys@users.noreply.github.com>
2019-03-09 00:11:13 +0000

    print from db to formatted, non related data

--- cm_approved_requests.php
+++ cm_approved_requests.php
@@ -4,7 +4,38 @@
 <?php require'includes/navbars/nav_picker.php';?>
 <!--This page should show all requests approved by the CM-->
 <!--TODO:  Make this display table & Add search function -->
+<?php
+$servername = "sql189.main-hosting.eu.";
+$username = "u359278202_eeuab";
+$password = "6N452fMvy6zc";
+$dbname = "u359278202_vawf";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$bedsTest = "SELECT * FROM user";
+$result = $conn->query($bedsTest);
+
+if ($result->num_rows > 0) {
+    echo"<h3>Test Output</h3>";
+    //echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $uname = $row["username"];
+        $passwd = $row["password"];
+        echo "<p><strong>Username:</strong> $uname<br><strong>Password:</strong> $passwd</p>";
+        //echo "<tr><td>" . $row["username"]. "</td><td>" . $row["password"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
 
 
+$conn->close();
+?>
 
 <?php require 'includes/footer.php';?>
