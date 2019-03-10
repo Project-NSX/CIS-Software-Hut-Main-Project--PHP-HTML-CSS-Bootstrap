26c7e0bc708f3c57dffabeaa8a9808eaafbe342b
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-03-10 10:12:19 +0000

    changed code to not repeate the db connection

--- cm_approved_requests.php
+++ cm_approved_requests.php
@@ -45,21 +45,13 @@
 <?php require'includes/navbars/nav_picker.php';?>
 <!--This page should show all requests approved by the CM-->
 <!--TODO:  Make this display table & Add search function -->
+
 <?php
+require_once'includes/database.php';
 
-$servername = "sql189.main-hosting.eu.";
-$username = "u359278202_eeuab";
-$password = "6N452fMvy6zc";
-$dbname = "u359278202_vawf";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 $bedsTest = "SELECT * FROM user";
-$result = $conn->query($bedsTest);
+$result = $link->query($bedsTest);
 
 if ($result->num_rows > 0) {
     echo"<h3>Test Output</h3>";
@@ -78,7 +70,7 @@ if ($result->num_rows > 0) {
 }
 
 
-$conn->close();
+$link->close();
 ?>
 
 <?php require 'includes/footer.php';?>
