de4974cc9b02827155d303f5ee98f20f6483146f
BedwyrPrys <44100623+BedwyrPrys@users.noreply.github.com>
2019-03-12 22:01:04 +0000

    improved forms ui

--- create_va.php
+++ create_va.php
@@ -11,7 +11,7 @@
         <fieldset>
             <legend>Name</legend>
             <label for="title">Title: </label>
-            <select name="title" id="title">
+            <select name="title" id="title" class="form-control">
                 <option value="mr">Mr</option>
                 <option value="miss">Miss</option>
                 <option value="mrs">Mrs</option>
@@ -20,46 +20,72 @@
                 <option value="prof">Prof</option>
                 <option value="other">Other</option>
             </select>
-            <label for="f_name">First Name: </label><input type="text" name="f_name">
-            <label for="l_name">Last Name: </label><input type="text" name="l_name">
+            <div class="form-row">
+            <div class="form-group col-md-6">
+            <label for="f_name">First Name: </label>
+            <input type="text" class="form-control" name="f_name">
+            </div>
+            <div class="form-group col-md-6">
+            <label for="l_name">Last Name: </label>
+            <input type="text" class="form-control" name="l_name">
+            </div>
+            </div>
         </fieldset>
 
-
-
-    <fieldset>
+        <fieldset>
         <legend>Address</legend>
-
-        <label for="street">Street: </label><input type="text" name="street">
-
-        <label for="town_city">Town / City: </label> <input type="text" name="town_city">
-
-        <label for="county">County: </label><input type="text" name="county">
-        <label for="postcode">Postcode: </label><input name="postcode"
-            pattern="[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}"
-            title="Please enter a valid UK postcode">
-
+        <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="street">Street: </label>
+        <input type="text" class="form-control" name="street">
+        </div>
+        <div class="form-group col-md-6">
+        <label for="town_city">Town / City: </label>
+        <input type="text" class="form-control" name="town_city">
+        </div>
+        </div>
+        <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="county">County: </label>
+        <input type="text" class="form-control" name="county">
+        </div>
+        <div class="form-group col-md-6">
+        <label for="postcode">Postcode: </label>
+        <input type="text" name="postcode" pattern="[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]? [0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}" title="Please enter a valid UK postcode" class="form-control">
+        </div>
+        </div>
     </fieldset>
+
     <fieldset>
         <legend>Contact Information</legend>
-
-        <label for="email">Email: </label> <input type="email" name="email">
-
-        <label for="phone_number">Phone Number: </label> <input type="tel" name="phone_number" minlength="9" maxlength="14">
-
+        <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="email">Email: </label>
+        <input type="email" class="form-control" name="email">
+        </div>
+        <div class="form-group col-md-6">
+        <label for="phone_number">Phone Number: </label>
+        <input type="tel" name="phone_number" class="form-control" minlength="9" maxlength="14">
+        </div>
+        </div>
     </fieldset>
+
     <fieldset>
-<legend>Academic Information</legend>
-<label for="type">Type of Academic: </label>
-            <select name="type" id="type">
-                <option value="mr">Undergraduate</option>
-                <option value="miss">PhD student</option>
-<!--TODO: Make the above two fields diaply an additional box for the user to enter additional information-->
-                <option value="mrs">Visiting Academic (Position)</option>
-                <option value="ms">Other (Specify)</option>
-                </select>
-                <label for="home_institution">Home Institution: </label><input type="text" name="home_institution">
-
+        <legend>Academic Information</legend>
+        <label for="type">Type of Academic: </label>
+            <select name="type" id="type" class="form-control">
+                <option value="undergrad">Undergraduate</option>
+                <option value="phd">PhD student</option>
+                <option value="vaPos">Visiting Academic (Position)</option>
+                <option value="otherSpecify">Other (Specify)</option>
+            </select>
+            <div class="form-row">
+            <label for="home_institution">Home Institution: </label>
+            <input type="text" class="form-control" name="home_institution">
+            </div>
     </fieldset>
-    <button>Send</button>
+
+    <button style="margin:10px 0px" type="submit" class="btn btn-primary btn-lg btn-block">Send</button>
+
 </form>
 <?php require 'includes/footer.php';?>
--- create_visit.php
+++ create_visit.php
@@ -6,36 +6,46 @@
     FIXME: Labels show up on the wrong line when changing window size
 -->
 <form method="post">
-
     <fieldset>
         <legend>Visitor</legend>
         <label for="Visitor">Visitor: </label>
-        <select name="visitor" id="visitor">
+        <select name="visitor" id="visitor" class="form-control">
             <option value="Select" disabled>Select</option>
         </select>
-
     </fieldset>
     <fieldset>
         <legend>Visit Dates</legend>
-        <label for="s_date">Start Date: </label><input type="date" name="s_date">
-        <label for="e_date">End Date: </label><input type="date" name="e_date">
+        <div class="form-row">
+        <div class="form-group col-md-6">
+        <label for="s_date">Start Date: </label>
+        <input type="date" name="s_date" class="form-control">
+        </div>
+        <div class="form-group col-md-6">
+        <label for="e_date">End Date: </label>
+        <input type="date" name="e_date" class="form-control">
+        </div>
+        </div>
     </fieldset>
+
     <fieldset>
         <legend>IPR Issues</legend>
-        <div>
-            <p>Are there IPR issues with the visit?</p>
-            <input type="radio" name="ipr_issues" value="yes">Yes<br>
-            <input type="radio" name="ipr_issues" value="no" checked> No
-            <!--TODO: Make attachment icon and message show if "yes" is selected above-->
+        <p>Are there IPR issues with the visit?</p>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="ipr_issues" id="inlineRadio1" value="yes">
+            <label class="form-check-label" for="inlineRadio1">Yes</label>
         </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="ipr_issues" id="inlineRadio1" value="no">
+            <label class="form-check-label" for="inlineRadio1">No</label>
+        </div>
+            <!--TODO: Make attachment icon and message show if "yes" is selected above-->
         <legend>Additional Info</legend>
-        <label for="summary">Summary of visit</label>: <textarea id="summary" name="summary" rows="4" cols="40"
-            placeholder="Please summarise the purpose of the visit" autofocus></textarea>
+        <div class="form-group">
+            <label for="summary">Summary of visit</label>
+            <textarea class="form-control" id="summary" name="summary" rows="4" cols="40" placeholder="Please summarise the purpose of the visit"></textarea>
+        </div>
     </fieldset>
-    <fieldset>
 
+    <button style="margin:10px 0px" type="submit" class="btn btn-primary btn-lg btn-block">Send</button>
 
-        <button>Send</button>
-
-
-        <?php require 'includes/footer.php';?>
+<?php require 'includes/footer.php';?>
