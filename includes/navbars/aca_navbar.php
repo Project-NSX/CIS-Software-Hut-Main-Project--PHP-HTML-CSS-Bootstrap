2bbc16f173dcb8aa230e711d25195a044f6dd3a6
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-04-24 14:03:16 +0100

    fixed navbar for academic

--- includes/navbars/aca_navbar.php
+++ includes/navbars/aca_navbar.php
@@ -5,7 +5,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item <?php if ($page =='AL'){echo 'active';}?>">
+            <li class="nav-item <?php if ($page =='home'){echo 'active';}?>">
                 <a class="nav-link" href="view_requests.php">Home<span class="sr-only"></span></a>
             </li>
             <?php require 'includes/navbars/va_tools.php';?>
