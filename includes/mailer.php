22a855eea6a5157a1fc1c106bc5e13f776474909
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-04-24 12:17:20 +0100

    Made mailer fit for purpose (i think)

--- includes/mailer.php
+++ includes/mailer.php
@@ -8,45 +8,49 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/PHPMailer/src/Exception.php';
-require '../vendor/PHPMailer/src/PHPMailer.php';
-require '../vendor/PHPMailer/src/SMTP.php';
+require 'vendor/PHPMailer/src/Exception.php';
+require 'vendor/PHPMailer/src/PHPMailer.php';
+require 'vendor/PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
-$mail ->isSMTP();
-$mail ->Host ='smtp.hostinger.com';
-$mail ->SMTPAuth = true;
-$mail ->Username = 'support@nwsd.online';
-$mail ->Password ='twNqxeX4okGE';
-$mail ->SMTPSecure = 'tls';
-$mail ->Port = 587;
+$mail->isSMTP();
+$mail->Host = 'smtp.hostinger.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'support@nwsd.online';
+$mail->Password = 'twNqxeX4okGE';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+$mail->setFrom('support@nwsd.online');
+$mail->Subject = 'New visit request that requires your attention';
+$mail->Body = 'A visit request has been made by $_SESSION["id"]. <br/> Please sign into the visiting academic form to respond to this.';
 
+//to get email for hr when college manager makes request
+// also used when visit is approved
+if ($_SESSION["role"] === "College Manager") {
+        $result = "SELECT email FROM user where role = 'Human Resources'";
 
-$mail ->setFrom('support@nwsd.online');
-$mail ->addAddress('spectre-nsx@outlook.com');
-$mail ->Subject = 'Stuff';
-$mail ->Body = 'This is a test';
+        $emailList = array();
+        while ($row = mysqli_fetch_array($result)) {
+                $emailList[] = $row;
+            }
+        foreach ($emailList as $email) {
+                $mail->addAddress("$email");
+            }
+    }
 
-// Put in the function to send the mail
+// to get email for cm when hos makes request
+if ($_SESSION["role"] === "Head Of School") {
+        $hosid = $_SESSION["college_id"];
+        $emailList = "SELECT email FROM user where college_id = '$hosid' AND role = 'College Manager'";
+        $mail->addAddress("$result");
+    }
 
- $mail ->send();
-?>
+// to get email for hos when academic makes request
+if ($_SESSION["role"] === "Academic") {
+        $aid = $_SESSION["school_id"];
+        $emailList = "SELECT email FROM user where school_id = '$aid' AND role = 'Head Of School'";
+        $mail->addAddress("$result");
+    }
 
-<!-- to get email for hr when college manager makes request -->
-<!-- also used when visit is approved -->
-<?php if ($_SESSION["role"] === "College Manager") {
-$CMsql = "SELECT email FROM user where role = 'Human Resources'";
-}?>
-
-<!-- to get email for cm when hos makes request -->
-<?php if ($_SESSION["role"] === "Head Of School") {
-$hosid = $_SESSION["college_id"];
-$HoSsql = "SELECT email FROM user where college_id = '$hosid' AND role = 'College Manager'";
-}?>
-
-<!-- to get email for hos when academic makes request -->
-<?php if ($_SESSION["role"] === "Academic") {
-$aid = $_SESSION["school_id"];
-$Asql = "SELECT email FROM user where school_id = '$hosid' AND role = 'Head Of School'";
-}?>
+$mail->send();
