0fb3199a10f9311513abb0680afd30b126aa1e19
JTomkins99 <jtomkins1999@gmail.com>
2019-04-26 17:20:28 +0100

    Add comments and alternative HR role redirect.

--- includes/verify_cm_role.php
+++ includes/verify_cm_role.php
@@ -1,4 +1,5 @@
 <?php
+    // Redirect users who aren't signed in as a user with the "College Manager" role.
     if(!isset($_SESSION["role"]) || (isset($_SESSION["role"]) && $_SESSION["role"] != "College Manager")) {
         header("location: index.php");
         exit;
--- includes/verify_hos_role.php
+++ includes/verify_hos_role.php
@@ -1,4 +1,5 @@
 <?php
+    // Redirect users who aren't signed in as a user with the "Head Of School" role.
     if(!isset($_SESSION["role"]) || (isset($_SESSION["role"]) && $_SESSION["role"] != "Head Of School")) {
         header("location: index.php");
         exit;
