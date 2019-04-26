ea6e73d8ab085ca23a7b198441e0fa9a89f7017c
JTomkins99 <jtomkins1999@gmail.com>
2019-04-26 19:12:50 +0100

    Fixed alternative role redirect for HR users.

--- includes/deny_hr_role.php
+++ includes/deny_hr_role.php
@@ -1,6 +1,6 @@
 <?php
     // Redirect users who are signed in as a user with the "Human Resources" role.
-    if(!isset($_SESSION["role"]) || (isset($_SESSION["role"]) && $_SESSION["role"] != "Human Resources")) {
+    if(!isset($_SESSION["role"]) || (isset($_SESSION["role"]) && $_SESSION["role"] == "Human Resources")) {
         header("location: index.php");
         exit;
     }
