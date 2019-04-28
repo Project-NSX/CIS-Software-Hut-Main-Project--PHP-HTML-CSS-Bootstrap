1e2d4b0537b3a2dc460a9e9a42c827b0c043effb
dafgri <eeub02@bangor.ac.uk>
2019-04-28 13:05:55 +0100

    Test commit
    checking if something works...

--- includes/footer.php
+++ includes/footer.php
@@ -3,5 +3,13 @@
 </body>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <!--TODO: Add a footer bar-->
-
+<footer>
+    <div id="footerInfo">
+        <p>Prifysgol Bangor University</p>
+        <p>Bangor, Gwynedd, LL57 2DG</p>
+        <p>Phone: 01248 351151</p>
+        <p>Email: marketing@bangor.ac.uk</p>
+        <p>Bangor University is a Registered Charity: No. 1141565</p>
+    </div>
+</footer>
 </html>
