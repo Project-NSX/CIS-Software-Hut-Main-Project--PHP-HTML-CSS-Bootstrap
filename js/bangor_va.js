0a36638338f4a4ca852868566889f3f7f42d9cba
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2019-04-29 12:17:38 +0100

    Revert "Deleted cookie banner JS"
    This reverts commit 461d360763e7fe417abe5411709cffeaf719bcaf.

--- js/bangor_va.js
+++ js/bangor_va.js
@@ -107,3 +107,7 @@ function updateDateFields() {
     else if (endField.value < startField.value)
         endField.setAttribute("value", startField.value);
 }
+
+// if($('.cookie-banner').length) {
+//     $('.cookie-banner').slideDown(10);
+// }
