c60b12372cdc01b980fe6b781873696feceb29f8
BedwyrPrys <44100623+BedwyrPrys@users.noreply.github.com>
2019-04-30 22:59:56 +0100

    updated code to use visitor

--- va_visit_details.php
+++ va_visit_details.php
@@ -10,14 +10,13 @@ require_once 'includes/database.php';
 
 $uname = $_SESSION["username"];
 
-$sql = "SELECT visitId FROM vaLogin WHERE username LIKE '" . $uname . "%'";
+$sql = "SELECT visitorId FROM vaLogin WHERE username LIKE '" . $uname . "%'";
             $result = $link->query($sql);
             while ($row = $result->fetch_assoc()) {
-                $visit = $row['visitId'];
+                $visit = $row['visitorId'];
             }
-
 //SQL statement to retrieve columns from database table
-$supervisorApproved = "SELECT v.visitId, v.visitorId, va.fName, va.lName, va.homeInstitution, va.department, va.email, va.phoneNumber, v.summary, v.visitAddedDate, v.status,  v.financialImplications, va.visitorType, va.visitorTypeExt,  v.startDate, v.endDate, v.supervisorApproved, v.supervisorUsername, v.supervisorApprovedDate, v.hrApproved, v.hrUsername, v.hrApprovedDate, v.induction, v.iprIssues, v.iprFile  FROM visit v, visitingAcademic va WHERE v.visitorId = va.visitorId AND v.visitId LIKE '" . $visit . "%' AND v.supervisorApproved LIKE '3' AND v.hrApproved LIKE '3' AND v.induction LIKE '0' ORDER BY v.visitAddedDate DESC";
+$supervisorApproved = "SELECT v.visitId, v.visitorId, va.fName, va.lName, va.homeInstitution, va.department, va.email, va.phoneNumber, v.summary, v.visitAddedDate, v.status,  v.financialImplications, va.visitorType, va.visitorTypeExt,  v.startDate, v.endDate, v.supervisorApproved, v.supervisorUsername, v.supervisorApprovedDate, v.hrApproved, v.hrUsername, v.hrApprovedDate, v.induction, v.iprIssues, v.iprFile  FROM visit v, visitingAcademic va WHERE v.visitorId = va.visitorId AND v.visitorId LIKE '" . $visit . "%' AND v.supervisorApproved LIKE '3' AND v.hrApproved LIKE '3' AND v.induction LIKE '0' ORDER BY v.visitAddedDate DESC";
 $supervisorApprovedresult = $link->query($supervisorApproved);
 if ($supervisorApprovedresult->num_rows > 0) {
 
