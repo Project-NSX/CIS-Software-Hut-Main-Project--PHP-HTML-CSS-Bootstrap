ea104b8e1018de951944875346e69097d1e74787
JTomkins99 <jtomkins1999@gmail.com>
2019-05-01 00:46:01 +0100

    Moved JS load to header from footer

--- includes/footer.php
+++ includes/footer.php
@@ -6,16 +6,6 @@
     });
 </script>
 </body>
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<!-- Application JavaScript -->
-<script src="js/bangor_va.js" type="text/javascript"></script>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <footer>
     <section class="footerContent">
         <div id="footerLeft">
