802a439718c1f3be19fc1a3caacbe5563b3619fe
JTomkins99 <jtomkins1999@gmail.com>
2019-05-01 01:54:17 +0100

    Fix modal interacting with the DB.

--- va_visit_details.php
+++ va_visit_details.php
@@ -17,9 +17,9 @@ while ($row = $result->fetch_assoc()) {
 }
 
 if (isset($_POST['acceptInduction'])) {
-        $accept = "UPDATE visit SET induction = 1, WHERE visitId = '$_POST[hidden]'";
-        mysqli_query($link, $accept);
-    };
+    $accept = "UPDATE visit SET induction = 1 WHERE visitId = '$_POST[hidden]'";
+    mysqli_query($link, $accept);
+}
 
 //SQL statement to retrieve columns from database table
 $supervisorApproved = "SELECT v.visitId, v.visitorId, va.fName, va.lName, va.homeInstitution, va.department, va.email, va.phoneNumber, v.summary, v.visitAddedDate, v.status,  v.financialImplications, va.visitorType, va.visitorTypeExt,  v.startDate, v.endDate, v.supervisorApproved, v.supervisorUsername, v.supervisorApprovedDate, v.hrApproved, v.hrUsername, v.hrApprovedDate, v.induction, v.iprIssues, v.iprFile  FROM visit v, visitingAcademic va WHERE v.visitorId = va.visitorId AND v.visitorId LIKE '" . $visit . "%' AND v.supervisorApproved LIKE '3' AND v.hrApproved LIKE '3' AND v.induction LIKE '0' ORDER BY v.visitAddedDate DESC";
@@ -64,84 +64,84 @@ if ($supervisorApprovedresult->num_rows > 0) {
         $iprIssues = $row['iprIssues'];
         $iprFile = $row['iprFile'];
         ?>
-<form action=va_visit_details.php method=post>
 
-        <!-- Using a card as an accordion -->
-        <div class="card">
-            <!-- Unique id and data target provided by database field which is unique, this is done so only one card expands on click rather than all of them -->
-            <div class="card-header" id="<?php echo $headingId ?>" <button id="button1" class="btn btn-link collapsed" data-toggle="collapse" data-target=" <?php echo $collapseIdHash ?>" aria-expanded="false" aria-controls=" <?php echo $collapseId ?>">
-                <div class="row">
-                    <div class='col-sm'><b><?php echo $lang['Name'] ?>: </b> <?php echo $fName . " " . $lName ?></div>
-                    <div class='col-sm'><b><?php echo $lang['Home Institution'] ?>: </b> <?php echo $homeInt ?></div>
-                    <div class='col-sm'><b><?php echo $lang['Department'] ?>: </b> <?php echo $department ?></div>
-                    <div class='col-sm'><b><?php echo $lang['Email'] ?>: </b> <?php echo $email ?></div>
-                    <div class='col-sm'><b><?php echo $lang['Phone Number'] ?>:</b> <?php echo $phone ?></div>
+        <form action="va_visit_details.php" method="post">
+            <!-- Using a card as an accordion -->
+            <div class="card">
+                <!-- Unique id and data target provided by database field which is unique, this is done so only one card expands on click rather than all of them -->
+                <div class="card-header" id="<?php echo $headingId ?>" <button id="button1" class="btn btn-link collapsed" data-toggle="collapse" data-target=" <?php echo $collapseIdHash ?>" aria-expanded="false" aria-controls=" <?php echo $collapseId ?>">
+                    <div class="row">
+                        <div class='col-sm'><b><?php echo $lang['Name'] ?>: </b> <?php echo $fName . " " . $lName ?></div>
+                        <div class='col-sm'><b><?php echo $lang['Home Institution'] ?>: </b> <?php echo $homeInt ?></div>
+                        <div class='col-sm'><b><?php echo $lang['Department'] ?>: </b> <?php echo $department ?></div>
+                        <div class='col-sm'><b><?php echo $lang['Email'] ?>: </b> <?php echo $email ?></div>
+                        <div class='col-sm'><b><?php echo $lang['Phone Number'] ?>:</b> <?php echo $phone ?></div>
+                    </div>
+                    <div class="row">
+                        <div class='col-md-1 offset-md-11' style="text-align: right;">&#x25BC</div>
+                    </div>
                 </div>
-                <div class="row">
-                    <div class='col-md-1 offset-md-11' style="text-align: right;">&#x25BC</div>
+                <div id="<?php echo $collapseId ?>" class="collapse" aria-labelledby="<?php echo $headingId ?>" data-parent="#accordion">
+                    <div class="card-body">
+                        <h5 class='card-title'><?php echo $lang['Visit Summary'] ?></h5>
+                        <p class='card-text'><?php echo htmlspecialchars($summary) ?></p>
+                        <h5 class='card-title'><?php echo $lang['Financial Implications'] ?></h5>
+                        <p class='card-text'><?php echo htmlspecialchars($financialImp) ?></p>
+                        <h5 class='card-title'><?php echo $lang['Visitor Type'] ?></h5>
+                        <p class='card-text'><?php echo $visitorType ?> &#8195; <?php echo htmlspecialchars($visitorTypeEXT) ?></p>
+                        <h5 class='card-title'><?php echo $lang['Visit Start & End Dates'] ?></h5>
+                        <p class='card-text'><b><?php echo $lang['Start'] ?>:</b> <?php echo $startDisplay ?> &#8195; <b><?php echo $lang['End'] ?>:</b> <?php echo $endDisplay ?></p>
+                        <h5 class='card-title'><?php echo $lang['Date & Time of Initial Submission'] ?></h5>
+                        <p class='card-text'><?php echo $addedDisplay ?> </p>
+                        <h5 class='card-title'><?php echo $lang['Supervisor Username'] ?></h5>
+                        <p class='card-text'><?php echo $supervisorUname ?> </p>
+                        <h5 class='card-title'><?php echo $lang['Date & Time of Decision'] ?></h5>
+                        <p class='card-text'><?php echo $supervisorApprovedDateDisp ?> </p>
+                        <h5 class='card-title'><?php echo $lang['HR Practitioner Username'] ?></h5>
+                        <p class='card-text'><?php echo $hrUname ?> </p>
+                        <h5 class='card-title'><?php echo $lang['Date & Time of Decision'] ?></h5>
+                        <p class='card-text'><?php echo $hrApprovedDateDisp ?> </p>
+
+                        <!-- if there is an IPR issue (field value = 1) - display the IPR deed file -->
+                        <?php
+                        if ($iprIssues == 1) {
+                            echo $lang['IPR'];
+                            echo "<p class='card-text'><a href='ipr/$iprFile' download>$iprFile</a>";
+                        }
+                        ?>
+                        <button type="button" onclick="updateHealthSafetyModal(<?php echo $visitId ?>)" class=" btn btn-primary" data-toggle="modal" data-target="#health-safety-dialog"> Open Modal </button>
+                    </div>
                 </div>
             </div>
-            <div id="<?php echo $collapseId ?>" class="collapse" aria-labelledby="<?php echo $headingId ?>" data-parent="#accordion">
-                <div class="card-body">
-                    <h5 class='card-title'><?php echo $lang['Visit Summary'] ?></h5>
-                    <p class='card-text'><?php echo htmlspecialchars($summary) ?></p>
-                    <h5 class='card-title'><?php echo $lang['Financial Implications'] ?></h5>
-                    <p class='card-text'><?php echo htmlspecialchars($financialImp) ?></p>
-                    <h5 class='card-title'><?php echo $lang['Visitor Type'] ?></h5>
-                    <p class='card-text'><?php echo $visitorType ?> &#8195; <?php echo htmlspecialchars($visitorTypeEXT) ?></p>
-                    <h5 class='card-title'><?php echo $lang['Visit Start & End Dates'] ?></h5>
-                    <p class='card-text'><b><?php echo $lang['Start'] ?>:</b> <?php echo $startDisplay ?> &#8195; <b><?php echo $lang['End'] ?>:</b> <?php echo $endDisplay ?></p>
-                    <h5 class='card-title'><?php echo $lang['Date & Time of Initial Submission'] ?></h5>
-                    <p class='card-text'><?php echo $addedDisplay ?> </p>
-                    <h5 class='card-title'><?php echo $lang['Supervisor Username'] ?></h5>
-                    <p class='card-text'><?php echo $supervisorUname ?> </p>
-                    <h5 class='card-title'><?php echo $lang['Date & Time of Decision'] ?></h5>
-                    <p class='card-text'><?php echo $supervisorApprovedDateDisp ?> </p>
-                    <h5 class='card-title'><?php echo $lang['HR Practitioner Username'] ?></h5>
-                    <p class='card-text'><?php echo $hrUname ?> </p>
-                    <h5 class='card-title'><?php echo $lang['Date & Time of Decision'] ?></h5>
-                    <p class='card-text'><?php echo $hrApprovedDateDisp ?> </p>
-
-                    <!-- if there is an IPR issue (field value = 1) - display the IPR deed file -->
-                    <?php
-                    if ($iprIssues == 1) {
-                        echo $lang['IPR'];
-                        echo "<p class='card-text'><a href='ipr/$iprFile' download>$iprFile</a>";
-                    }
-                    ?>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#health-safety-dialog"> Open Modal </button>
-                </div>
-            </div>
-        </div>
-        <br>
-    <?php
-}
-echo "</div>";
+            <br>
+        <?php
+    }
+    echo "</div>";
 } else { }
 
 $link->close();
 ?>
 
-<div class="modal fade" id="health-safety-dialog" tabindex="-1" role="dialog" aria-labelledby="health-safety-title" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="health-safety-title"><?php echo $lang["H&S Induction"] ?></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+    <div class="modal fade" id="health-safety-dialog" tabindex="-1" role="dialog" aria-labelledby="health-safety-title" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="health-safety-title"><?php echo $lang["H&S Induction"] ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p><?php echo $lang["H&S Intro"] ?></p>
+                    <a href="<?php echo $lang["H&S Policy Link"] ?>" target="_blank"><?php echo $lang["H&S Link"] ?></a>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="hidden" id="health-safety-visit" value="<?php echo $visitId ?>">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php echo $lang["Cancel"] ?></button>
+                    <input type="submit" name="acceptInduction" class="btn btn-primary" value="<?php echo $lang["Proceed"] ?>"></button>
+                </div>
             </div>
-        <div class="modal-body">
-            <p><?php echo $lang["H&S Intro"] ?></p>
-            <a href="<?php echo $lang["H&S Policy Link"] ?>" target="_blank"><?php echo $lang["H&S Link"] ?></a>
         </div>
-        <div class="modal-footer">
-        <input type=hidden name=hidden value=<?php echo $visitId ?>>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php echo $lang["Cancel"] ?></button>
-            <input type=submit name=acceptInduction class="btn btn-primary" value="<?php echo $lang["Proceed"] ?>"></button>
-        </div>
-    </div>
     </div>
 </form>
-</div>
 <?php require 'includes/footer.php'; ?>
