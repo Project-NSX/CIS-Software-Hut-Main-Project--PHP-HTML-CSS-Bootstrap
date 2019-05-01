325c51945930a17c6b41f4b2048083df5c889958
dafgri <eeub02@bangor.ac.uk>
2019-05-01 04:04:07 +0100

    Footer Edited

--- includes/footer.php
+++ includes/footer.php
@@ -12,13 +12,12 @@
         <div id="footerLeft">
         </div>
         <div id="footerRight">
-            <p>Prifysgol Bangor University</p>
+            <p>© 2019 Prifysgol Bangor University</p>
             <p>Bangor, Gwynedd, LL57 2DG</p>
             <p><?php echo $lang['PhoneNo'] ?></p>
             <p><?php echo $lang['ContactEmail'] ?></p>
             <p><?php echo $lang['Charity'] ?></p>
-            <a href="index.php?lang=en"><?php echo $lang['lang_en'] ?></a>
-            | <a href="index.php?lang=cy"><?php echo $lang['lang_cy'] ?></a>
+            <a href="index.php?lang=en"><?php echo $lang['lang_en'] ?></a> | <a href="index.php?lang=cy"><?php echo $lang['lang_cy'] ?></a>
         </div>
     </section>
 </footer>
