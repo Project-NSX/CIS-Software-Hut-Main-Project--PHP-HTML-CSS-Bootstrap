bbbd2d798b9a66c669152d90b508a7c19a3630be
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2021-01-28 12:46:25 +0000

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -2,15 +2,10 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-define('DB_SERVER', 'sql189.main-hosting.eu.');
-define('DB_USERNAME', 'u359278202_eeuab');
-define('DB_PASSWORD', '6N452fMvy6zc');
-define('DB_NAME', 'u359278202_vawf');
-
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', 'MyNewPass');
-// define('DB_NAME', 'va_form');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'MyNewPass');
+define('DB_NAME', 'va_form');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -27,10 +22,10 @@ if ($link === false) {
  */
 function getDB()
 {
-    $db_host = "sql189.main-hosting.eu.";
-    $db_name = "u359278202_vawf";
-    $db_user = "u359278202_eeuab";
-    $db_pass = "6N452fMvy6zc";
+    $db_host = "127.0.0.1";
+    $db_name = "va_form";
+    $db_user = "root";
+    $db_pass = "MyNewPass";
 
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
